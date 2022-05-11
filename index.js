a11d290ebb322791c8a3d86dfb02cdfa2dda6c12
lys22 <105361609+lys22@users.noreply.github.com>
2022-05-11 20:16:00 +0800

    Add files via upload
    enen

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||周老师|嘿嘿|哥哥|祝你|生日|快乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||周老师|♥|哥哥|今天是|512|祝你|生日|快乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
